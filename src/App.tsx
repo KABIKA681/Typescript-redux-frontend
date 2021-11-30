2a87f2bae871978aa439db1ae7d0dc92973563af
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-30 10:18:05 +0200

    tailwind-configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Routes1 from './Routes1';
-import Sidebar from './pages/SideBar/Sidebar';
+import Sidebar from './components/Sidebar';
 import { createTheme } from '@material-ui/core'
 import {ThemeProvider} from '@material-ui/styles'
 
