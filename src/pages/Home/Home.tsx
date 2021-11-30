0b65b4b5e8a24aa926e92142011a3db74bc86173
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-30 11:49:47 +0200

    light-and-darkMode

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Button from '@mui/material/Button';
+import { BeakerIcon } from '@heroicons/react/solid'
 
 
 const  Home = () => {
     return (
         <div className='home'>
             Home page
-             <Button variant="contained">Hello World</Button>
+            <Button variant="contained">Hello World</Button>
+            <BeakerIcon className="h-5 w-5 text-blue-500"/>
+
 
         </div>
     )
