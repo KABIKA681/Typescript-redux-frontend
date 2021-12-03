39ff324e35485bc9b8589cd3ef60739f28f96e70
KABIKA681 <pascalkabikampan@gmail.com>
2021-12-04 00:27:36 +0200

    feedbackReducers

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,12 @@
 import { CountryReducerState } from './CountryTypes'
-
+import { BuzReducerState } from './FeedbackTypes'
 
 export * from './CountryTypes'
+export * from './FeedbackTypes'
 
 //global App State
 
 export type AppState = {
     countryReducer: CountryReducerState
+    feedbackReducer: BuzReducerState
 }
