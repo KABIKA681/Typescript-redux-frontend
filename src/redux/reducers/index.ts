d13807fa9466b60833a326e2d6230c547967b602
KABIKA681 <pascalkabikampan@gmail.com>
2021-12-04 00:18:43 +0200

    feedbacksTypes

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,13 @@
 import {combineReducers} from 'redux'
 
 import countryReducer from './CountryReducer'
+import feedbackReducer from './FeedbackReducer'
+
 
 const rootReducer = () => 
     combineReducers({
-        countryReducer
+        countryReducer,
+        feedbackReducer
     })
 
 
---
+++ src/types/FeedbackTypes.ts
@@ -0,0 +1,21 @@
+//reducer case constants
+export const ADD_COUNTRY_TO_BUZ = 'ADD_COUNTRY_TO_BUZ'
+export const REMOVE_COUNTRY_FROM_BUZ='REMOVE_COUNTRY_FROM_BUZ'
+
+//types
+export type BuzReducerState = {
+    buz: [],
+    
+}
+
+// action types
+export type AddCountryToBuz = {
+    type: typeof ADD_COUNTRY_TO_BUZ
+    payload: object
+}
+export type RemoveCountryFromBuz= {
+    type: typeof REMOVE_COUNTRY_FROM_BUZ
+    payload: object
+}
+
+export type FeedbackActions = AddCountryToBuz | RemoveCountryFromBuz 
